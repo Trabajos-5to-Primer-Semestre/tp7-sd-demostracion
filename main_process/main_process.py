b232fcc0fe6c6ef7edcc7de624aba96604fb7e6d
octav <15966899+OrionVi1998@users.noreply.github.com>
2023-05-01 11:30:06 -0300

    Add mongoDB connection to main_process.py

--- main_process/main_process.py
+++ main_process/main_process.py
@@ -4,6 +4,7 @@ import numexpr
 import socket
 import _thread
 import threading
+import pymongo
 
 PORT = 6666
 
@@ -14,10 +15,14 @@ def receive_utf_msg(socket_connection):
 
     return msg
 
-
 def send_msg_utf(socket_connection, msg):
     socket_connection.send(len(msg).to_bytes(2, byteorder='big'))
     socket_connection.send(bytes(msg, encoding="UTF-8"))
+
+
+mongoClient = pymongo.MongoClient('mongoDB', 27017)
+mongoDB = mongoClient['calculos']
+calculosCol = mongoDB['calculos']
 
 
 def client_logic(socket_connection):
@@ -31,16 +36,24 @@ def client_logic(socket_connection):
         print(f"Thread {thread_id}: Recibido: {data}")
 
         while True:
-            calculo = receive_utf_msg(socket_connection)
+            calculo_rec = receive_utf_msg(socket_connection)
 
-            if len(calculo) == 0:
+            if len(calculo_rec) == 0:
                 break
 
-            print(f"Thread {thread_id}: Recibido: {calculo}")
-            calculo = str(numexpr.evaluate(calculo.replace('^', '**')))
-            print(f'Thread {thread_id}: Respondiendo:{calculo}')
+            print(f"Thread {thread_id}: Recibido: {calculo_rec}")
 
-            send_msg_utf(socket_connection, calculo)
+            dbResponse = calculosCol.find_one({"calculo":{ '$eq': calculo_rec }})
+
+            if dbResponse:
+                print(f'Thread {thread_id}: Respondiendo desde DB:{calculo_rec}')
+                send_msg_utf(socket_connection, dbResponse['response'])
+            else:
+                calculo = str(numexpr.evaluate(calculo_rec.replace('^', '**')))
+                calculosCol.insert_one({"calculo":calculo_rec, "response":calculo})
+                print(f'Thread {thread_id}: Respondiendo:{calculo}')
+
+                send_msg_utf(socket_connection, calculo)
 
 
 
